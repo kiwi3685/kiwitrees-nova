faa7e8e50c3719c0105de2e86d3f2d3f7dfd5dbb
kiwi3685 <kiwi3685@gmail.com>
2021-11-02 16:14:24 +1300

    Improve button layout

--- modules_v4/chart_pedigree/module.php
+++ modules_v4/chart_pedigree/module.php
@@ -157,7 +157,9 @@ class chart_pedigree_KT_Module extends KT_Module implements KT_Module_Chart {
 							</div>
 						</div>
 					</div>
-					<div class="cell medium-1 buttonRow">
+					<div class="cell medium-1 buttonRow
+">
+						<p></p>
 						<button class="button" type="submit">
 							<i class="<?php echo $iconStyle; ?> fa-eye"></i>
 							<?php echo KT_I18N::translate('Show'); ?>
