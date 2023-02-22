ba846afb840c1116f4c64818177050e901098a03
kiwi3685 <Kiwi3685@gmail.com>
2023-02-22 14:50:52 +1300

    Add en-US language file to datepicker

---
+++ library/foundation-datepicker/js/locales/foundation-datepicker.en-US.js
@@ -0,0 +1,17 @@
+/**
+ * US English translation for foundation-datepicker
+ *
+ */
+;(function($){
+	$.fn.fdatepicker.dates['en-GB'] = {
+		days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+		daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+		daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+		months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+		monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+		today: "Today",
+		clear: "Clear",
+		weekStart: 1,
+		format: "mm/dd/yyyy"
+	};
+}(jQuery));
