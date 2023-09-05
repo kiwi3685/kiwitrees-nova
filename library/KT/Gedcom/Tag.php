7d77e04d69c525b12c4e3f63debc37e19f32cde8
kiwi3685 <Kiwi3685@gmail.com>
2023-09-06 10:39:27 +1200

    PHP 8.2 string null deprecated error

--- library/KT/Gedcom/Tag.php
+++ library/KT/Gedcom/Tag.php
@@ -934,7 +934,7 @@ class KT_Gedcom_Tag {
 			case '__IMAGE_SIZE__': return KT_I18N::translate('Image dimensions');
 			default:
 				// If no specialisation exists (e.g. DEAT:CAUS), then look for the general (CAUS)
-				if (strpos($tag, ':')) {
+				if (strpos((string) $tag, ':')) {
 					list(, $tag)=explode(':', $tag, 2);
 					return self::getLabel($tag, $record);
 				}
